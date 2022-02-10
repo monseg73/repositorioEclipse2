d3e343459a911acab34619a50c610c442013a05d
monseg73 <monsegcarretero@gmail.com>
2022-02-10 14:13:48 +0100

    Valido cambio de try..catch

--- Sumar/src/suma/Suma.java
+++ Sumar/src/suma/Suma.java
@@ -12,26 +12,31 @@ public class Suma {
 	public static void suma() {
 		int a,b, resultado;
 		String respuesta="s";
+		try {
+			Scanner teclado =new Scanner(System.in);
+			while (respuesta=="s"){
+				System.out.println("Introduce un número positivo:");
+				a=teclado.nextInt();
+				System.out.println("Introduce otro número positivo:");
+				b=teclado.nextInt();
+				if ((a > 0) && (b >0)) {
+					resultado= a+b;
+					System.out.println("El resultado es " + resultado);
+					
+				}
+				else {
+					System.out.println("No son positivos");
+				}
+				System.out.println("¿Quieres seguir sumando? s/n");
+				respuesta=teclado.next();
+			}
+			teclado.close();
 		
-		Scanner teclado =new Scanner(System.in);
-		while (respuesta=="s"){
-			System.out.println("Introduce un número positivo:");
-			a=teclado.nextInt();
-			System.out.println("Introduce otro número positivo:");
-			b=teclado.nextInt();
-			if ((a > 0) && (b >0)) {
-				resultado= a+b;
-				System.out.println("El resultado es " + resultado);
-				
-			}
-			else {
-				System.out.println("No son positivos");
-			}
-			System.out.println("¿Quieres seguir sumando? s/n");
-			respuesta=teclado.next();
 		}
-		teclado.close();
-		
+		catch (Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
 		
 	}
 }
